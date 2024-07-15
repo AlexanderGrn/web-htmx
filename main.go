1552528f57508ec6a646078e5ca36cd9242c4a00
AlexanderGrn <s.green@list.ru>
2024-07-15 16:04:06 +0300

    feat: create simple server

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
 func main() {
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello World!")
+		io.WriteString(w, "\n"+r.Method)
+	}
+	http.HandleFunc("/", h1)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
